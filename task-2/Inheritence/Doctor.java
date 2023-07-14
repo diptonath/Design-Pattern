ec0ce9b294b3ea83103f6a2522c6c9348b6a5419
Dipto Nath <45964738+diptonath@users.noreply.github.com>
2023-07-14 23:00:10 +0600

    task 2

---
+++ task-2/Inheritence/Doctor.java
@@ -0,0 +1,10 @@
+
+public class Doctor extends Human {         //Inheritance
+
+    public void howToLoseFat() {
+
+        System.out.println("Well make sure you exercise.");
+    }
+    
+
+}
---
+++ task-2/Inheritence/Singer.java
@@ -0,0 +1,11 @@
+
+public class Singer extends Human {           //Inheritance
+
+    public void singASong() {
+
+        System.out.println("Ek din aap you hamko mil jayenge");
+    }
+
+
+}
+
