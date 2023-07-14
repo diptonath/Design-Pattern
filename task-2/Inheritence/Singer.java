900dde4b9fd89266cb74dd70d59536a7a18482f2
Dipto Nath <45964738+diptonath@users.noreply.github.com>
2023-07-14 23:03:32 +0600

    update

--- task-2/Inheritence/Singer.java
+++ task-2/Inheritence/Singer.java
@@ -1,6 +1,7 @@
 
-public class Singer extends Human {           //Inheritance
-
+public class Singer extends Human {           
+    
+    //Inheritance
     public void singASong() {
 
         System.out.println("Ek din aap you hamko mil jayenge");
--- task-2/Inheritence/Teacher.java
+++ task-2/Inheritence/Teacher.java
@@ -1,9 +1,11 @@
 
-public class Teacher extends Human {          //Inheritance
+public class Teacher extends Human {          
+    //Inheritance
     
     public void givesUsALesson() {
 
         System.out.println("Dream is not the thing you see in sleep but is that thing that doesn't let you sleep");
     }
+
 }
 
